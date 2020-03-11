e33cc7ffae068c645c95856fea0800afe7838e8f
Comatoznik <58427696+Comatoznik@users.noreply.github.com>
2020-03-11 22:32:50 +0300

    Калькулятор V 1.0

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Alef", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(174, 31);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.AcceptsReturn = true;
+            this.textBox2.Font = new System.Drawing.Font("Alef", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(278, 12);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(183, 31);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(12, 217);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(72, 56);
+            this.button14.TabIndex = 25;
+            this.button14.Text = "*";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.generation_Click);
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(246, 217);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(72, 56);
+            this.button15.TabIndex = 27;
+            this.button15.Text = "/";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.splitting_Click);
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(168, 217);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(72, 56);
+            this.button16.TabIndex = 29;
+            this.button16.Text = "--";
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(90, 217);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(72, 56);
+            this.button17.TabIndex = 31;
+            this.button17.Text = "+";
+            this.button17.UseVisualStyleBackColor = true;
+            this.button17.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // button3
+            // 
+            this.button3.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.button3.Location = new System.Drawing.Point(324, 217);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(137, 58);
+            this.button3.TabIndex = 32;
+            this.button3.Text = "Очистка";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(105, 86);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(246, 22);
+            this.textBox3.TabIndex = 33;
+            // 
+            // button4
+            // 
+            this.button4.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.button4.Location = new System.Drawing.Point(128, 136);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(190, 59);
+            this.button4.TabIndex = 34;
+            this.button4.Text = "Увидеть результат";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.equal_Click);
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.SystemColors.Control;
+            this.label5.Location = new System.Drawing.Point(213, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 28);
+            this.label5.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(473, 287);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Калькулятор Самылкина";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public char F;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void generation_Click(object sender, EventArgs e)
+        {
+            F = Convert.ToChar("*");
+            label5.Text = Convert.ToString(F);
+
+        }
+        private void plus_Click(object sender, EventArgs e)
+        {
+            F = Convert.ToChar('+');
+            label5.Text = Convert.ToString(F);
+        }
+
+        public void minus_Click(object sender, EventArgs e)
+        {
+            F = Convert.ToChar('-');
+            label5.Text = Convert.ToString(F);
+        }
+
+        private void splitting_Click(object sender, EventArgs e)
+        {
+            F = Convert.ToChar('/');
+            label5.Text = Convert.ToString(F);
+        }
+
+        private void equal_Click(object sender, EventArgs e)
+        {
+            if (F == '+')
+                textBox3.Text = Convert.ToString(Convert.ToInt32(textBox1.Text) + Convert.ToInt32(textBox2.Text));
+            if (F == '*')
+                textBox3.Text = Convert.ToString(Convert.ToInt32(textBox1.Text) * Convert.ToInt32(textBox2.Text));
+            if (F == '-')
+                textBox3.Text = Convert.ToString(Convert.ToInt32(textBox1.Text) - Convert.ToInt32(textBox2.Text));
+            if (F == '/')
+                textBox3.Text = Convert.ToString(Convert.ToInt32(textBox1.Text) / Convert.ToInt32(textBox2.Text));
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            F = '0';
+        }
+    }
+}
